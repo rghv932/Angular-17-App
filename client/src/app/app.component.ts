e24da52c31a3e9fabe0c1a60b5bdcd93c23cc6bc
Raghav Maheshwari <raghavrocks932@gmail.com>
2025-06-24 22:19:41 +0530

    Init Commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  http = inject(HttpClient);
+  title = 'client';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7190/api/users').subscribe({
+      next: response => this.users = response,
+      error: err => console.log('error occurred:', err),
+      complete: () => console.log('Request has completed')
+    })
+  }
+}

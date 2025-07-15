3526106b0d5117676a1ce4996a9794a4a2b03b79
Raghav Maheshwari <raghavrocks932@gmail.com>
2025-07-15 23:20:53 +0530

    Reactive Forms

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -18,8 +18,8 @@ import { Photo } from '../../_models/photo';
 })
 export class PhotoEditorComponent implements OnInit {
   member = input.required<Member>();
-  private accountService= inject(AccountService);
-  private memberService= inject(MembersService);
+  private accountService = inject(AccountService);
+  private memberService = inject(MembersService);
   uploader?: FileUploader;
   hasBaseDropzoneOver = false;
   baseUrl = environment.apiUrl;
@@ -38,11 +38,11 @@ export class PhotoEditorComponent implements OnInit {
     this.memberService.setMainPhoto(photo).subscribe({
       next: _ => {
         const user = this.accountService.currentUser();
-        if(user){
+        if (user) {
           user.photoUrl = photo.url;
           this.accountService.setCurrentUser(user);
         }
-        const updatedMember = {...this.member()};
+        const updatedMember = { ...this.member() };
         updatedMember.photoUrl = photo.url;
         updatedMember.photos.forEach(p => {
           if (p.isMain) p.isMain = false;
@@ -51,12 +51,12 @@ export class PhotoEditorComponent implements OnInit {
         this.memberChange.emit(updatedMember);
       }
     });
-  } 
+  }
 
   deletePhoto(photo: Photo) {
     this.memberService.deletePhoto(photo).subscribe({
       next: _ => {
-        const updatedMember = {...this.member()};
+        const updatedMember = { ...this.member() };
         updatedMember.photos = updatedMember.photos.filter(x => x.id !== photo.id);
         this.memberChange.emit(updatedMember);
       }
@@ -81,16 +81,22 @@ export class PhotoEditorComponent implements OnInit {
     this.uploader.onSuccessItem = (item, response, status, headers) => {
       if (response) {
         const photo: Photo = JSON.parse(response);
-        const updatedMember = {...this.member()}
+        const updatedMember = { ...this.member() }
         updatedMember.photos.push(photo);
-        //  if (photo.isMain) {
-        //    this.user.photoUrl = photo.url;
-        //    this.member.photoUrl = photo.url;
-        //    this.accountService.setCurrentUser(this.user);
-        //  }
-        this.memberChange.emit(updatedMember);
+        if (photo.isMain) {
+          const user = this.accountService.currentUser();
+          if (user) {
+            user.photoUrl = photo.url;
+            this.accountService.setCurrentUser(user);
+          }
+          updatedMember.photoUrl = photo.url;
+          updatedMember.photos.forEach(p => {
+            if (p.isMain) p.isMain = false;
+            if (p.id === photo.id) p.isMain = true;
+          });
+          this.memberChange.emit(updatedMember);
+        }
       }
     }
   }
-
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,31 +1,45 @@
 import { Component, inject, OnInit, output } from '@angular/core';
-import { AbstractControl, FormGroup, FormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { 
+  AbstractControl, 
+  FormBuilder, 
+  FormGroup, 
+  FormsModule, 
+  ReactiveFormsModule, 
+  ValidatorFn, 
+  Validators 
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
 
 import { ToastrService } from 'ngx-toastr';
 
 import { AccountService } from '../_services/account.service';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
+import { DateInputComponent } from '../_forms/date-picker/date-picker.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, TextInputComponent, ReactiveFormsModule, NgIf, NgFor, DateInputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit {
   private accountService = inject(AccountService);
   private toastService = inject(ToastrService);
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
   cancelRegister = output<boolean>();
-  registerForm: FormGroup;
-  maxDate: Date;
-  validationErrors: string[] = [];
+  registerForm: FormGroup = new FormGroup({});
+  maxDate = new Date();
+  validationErrors: string[] | undefined = [];
 
   // constructor(private accountService:AccountService,,
   //   private fb:FormBuilder,private router:Router) { }
 
   ngOnInit(): void {
     this.intitializeForm();
-    this.maxDate = new Date();
+    // this.maxDate = new Date();
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
@@ -41,24 +55,29 @@ export class RegisterComponent implements OnInit {
       Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: _ => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    });
   }
 
   matchValues(matchTo: string): ValidatorFn {
     return (control: AbstractControl) => {
-      return control?.value === control?.parent?.controls[matchTo].value
+      return control?.value === control?.parent?.get(matchTo)?.value
         ? null : { isMatching: true }
     }
   }
 
   register() {
-    this.accountService.register(this.registerForm.value).subscribe(
+    const dob = this.getDateOnly(this.registerForm.get('dateOfBirth')?.value);
+    this.registerForm.patchValue({dateOfBirth: dob});
+    this.accountService.register(this.registerForm?.value).subscribe(
       {
-        next: response => {
-          // this.router.navigateByUrl('/members');
-          this.cancel();
+        next: _ => {
+          this.router.navigateByUrl('/members');
+          // this.cancel();
         },
         error: error => {
-          this.toastService.error(error.error)
+          // this.toastService.error(error.error)
           this.validationErrors = error;
         }
       });
@@ -66,5 +85,10 @@ export class RegisterComponent implements OnInit {
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  private getDateOnly(dob: string | undefined){
+    if(!dob) return;
+    return new Date(dob).toISOString().slice(0, 10);
   }
 }
